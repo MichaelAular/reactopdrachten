aa4feef3bbf985427dea0c0509d9bce923932312
Michael Aular <michaelaular@gmail.com>
2022-01-18 09:15:16 +0100

    to do

--- src/pages/Opdracht4.js
+++ src/pages/Opdracht4.js
@@ -7,7 +7,6 @@ import { useEffect } from "react/cjs/react.development";
 const Opdracht4 = () => {
   const [tasks, setTasks] = useState([]);
   const [userInput, setUserInput] = useState('');
-
   const addTask = () => {
     const newTask = {
       id: tasks.length + 1,
@@ -33,21 +32,20 @@ const Opdracht4 = () => {
 
   function CheckTask() {
     console.log('check');
-    const check = document.querySelector('.check');
+    const check = document.querySelector('.checked');
     check.classList.add('checked');
     check.classList.remove('unchecked');
-}
+  }
 
   return (
     <main>
       <h2> The To-Do-List</h2>
       <input id="inputTask" onChange={changeHandler} placeholder="Things I Need To Do" />
       <button onClick={addTask}>Submit To Do!</button>
-
       <ul className="list">
         {tasks.map((item) =>
           <li key={item.key} status={item.status}>
-              <FaCheck className="icons check unchecked" id={uuidv4()} role="button" onClick={CheckTask}/>
+              <FaCheck className="icons unchecked" id={uuidv4()} role="button" onClick={CheckTask}/>
               {item.task}
               <FaTrashAlt className="icons delete" key={item.key} role="button" onClick={ClearTask}/>
           </li>)}
